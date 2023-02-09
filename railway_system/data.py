65fbb651373a838b55c632c4bb1d38a80b1f6b50
ivangorcanuk <ivangorcanuk464@gmail.com>
2023-02-09 23:51:24 +0300

    Added methods for creating train and route objects, as well as a method for filling the list with the required number of digits.

--- railway_system/data.py
+++ railway_system/data.py
@@ -1,19 +1,28 @@
-from train_tupe import Passenger, Cargo
+from train_tupe import Passenger, Cargo, Schedule
 
 
 class WorkingUtils:
 
-    @staticmethod
-    def registration_train(number, nickname, train_type, max_capacity_pas, max_weight, type_passenger_train, count_wagons):
-        obj = None
-        if train_type == 'Пассажирский':
-            obj = Passenger(number, nickname, train_type, max_capacity_pas, type_passenger_train, count_wagons)
-        elif train_type == 'Грузовой':
-            obj = Cargo(number, nickname, train_type, max_weight, count_wagons)
+    @staticmethod  # регистрация поезда
+    def registration_train(number, nickname, train_type, max_capacity_pas, max_weight,
+                           type_passenger_train, count_wagons, average_speed, type_wagons):
+        train = train_type == 'Пассажирский'
+        if train:
+            obj = Passenger(number, nickname, train_type, max_capacity_pas, type_passenger_train, count_wagons, average_speed)
+        else:
+            obj = Cargo(number, nickname, train_type, max_weight, count_wagons, average_speed, type_wagons)
+        return obj
+
+    @staticmethod  # создание расписания
+    def registration_schedule(nickname, date_departures, date_arrival, time_departures, time_arrival, time_travel, train_type):
+        obj = Schedule(nickname, date_departures, date_arrival, time_departures, time_arrival, time_travel, train_type)
         return obj
 
     @staticmethod
-    def formation_train():
-        pass
-        # list_count_wagons = list()
-        # for i in
+    def count_num(num):
+        list_numbers = list()
+        i = 0
+        while i < num:
+            i += 1
+            list_numbers.append(i)
+        return list_numbers
