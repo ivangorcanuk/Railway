fa11beb9be98477cb4a55fe58608211fec156da6
ivangorcanuk <ivangorcanuk464@gmail.com>
2023-02-09 23:47:19 +0300

    Added a function for reading information from SQLite

--- railway_system/SQLite.py
+++ railway_system/SQLite.py
@@ -8,11 +8,15 @@ class SQL:
         self.cursor = self.conn.cursor()
 
     def select_cities(self):
-        list_city = list()
-        self.cursor.execute('''SELECT city FROM city''')
+        dict_city = {}
+        self.cursor.execute('''SELECT city, geo_lat, geo_lon FROM city''')
         element = self.cursor.fetchall()
         for city in element:
-            list_city.append(city)
+            dict_city[city[0]] = city[1], city[2]
         self.cursor.close()
 
-        return list_city
+        return dict_city
+
+# sql = SQL()
+# for key, value in sql.select_cities().items():
+#     print(key, value)
