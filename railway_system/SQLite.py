ec87b195c1fd68e2e18812eb57def4cf5e55bbdb
ivangorcanuk <ivangorcanuk464@gmail.com>
2023-02-20 17:49:19 +0300

    Fixed inaccuracies.

--- railway_system/SQLite.py
+++ railway_system/SQLite.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-class SQL:
+class DBhandler:
 
     def __init__(self):
         self.conn = sqlite3.connect('railway_system.bd')
@@ -13,27 +13,20 @@ class SQL:
         element = self.cursor.fetchall()
         for city in element:
             dict_city[city[0]] = city[1], city[2]
-        self.cursor.close()
 
         return dict_city
 
     def insert_train(self, train):
         self.cursor.execute('''INSERT INTO train
-                            (nickname, train_type, type_wagon, max_pas/weight, count_wagons, average_speed)
+                            (nickname, train_type, type_wagon, max_capacity, count_wagons, average_speed)
                             VALUES (?,?,?,?,?,?)''',
-                            (train.nickname, train.train_type, train.type_wagons, train.max, train.count_wagons, train.average_speed))
+                            (train.nickname, train.train_type, train.type_wagons, train.max_load, train.count_wagons, train.average_speed))
         self.conn.commit()
-        self.cursor.close()
 
-    def insert_schedule(self, schedule):
+    def insert_schedule(self, schedule, train):
         self.cursor.execute('''INSERT INTO schedule
-                            (nickname, date_sending, date_arrival, time_sending, time_arrival, time_travel, train_type)
-                            VALUES (?,?,?,?,?,?,?)''',
-                            (schedule.nickname, schedule.date_departures, schedule.date_arrival, schedule.time_departures,
-                             schedule.time_arrival, schedule.time_travel, schedule.train_type))
+                            (nickname, date_sending, time_sending, time_arrival, time_travel, train_type)
+                            VALUES (?,?,?,?,?,?)''',
+                            (train.nickname, schedule.date_sending, str(schedule.time_sending),
+                             str(schedule.time_arrival), str(schedule.time_travel), train.train_type))
         self.conn.commit()
-        self.cursor.close()
-
-# sql = SQL()
-# for key, value in sql.select_cities().items():
-#     print(key, value)
